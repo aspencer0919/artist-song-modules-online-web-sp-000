aa4bf2a13b2f90885047d1116cbaf618da18c1f4
Adam Spencer <aspencer0919@gmail.com>
2020-05-07 22:15:06 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 module Memorable
   module ClassMethods
-    def self.reset_all
+    def reset_all
       self.all.clear
     end
 
-    def self.count
+    def count
       self.all.count
     end
   end
